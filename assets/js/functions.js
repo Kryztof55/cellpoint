c3d1bf9dbc43126cf9a80cc43260e683afaedc89
Kryztof55 <c.farango.dg@gmail.com>
2020-08-02 14:47:17 -0500

    Se atienden cambios

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -10,6 +10,14 @@ $(window).scroll(function (event) {
 if ($(window).scrollTop() > 0) {
   navBar.addClass("color");
 }
+
 $(document).ready(function () {
   AOS.init();
+  $("a.scrollLink").click(function (event) {
+    event.preventDefault();
+    $("html, body").animate(
+      { scrollTop: $($(this).attr("href")).offset().top },
+      500
+    );
+  });
 });
